9d3591bfa8e41d1cec7beb03f3c9841086c62f06
irkham04 <59617595+irkham04@users.noreply.github.com>
2025-09-01 01:22:02 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '9d23b9af-4fe1-4b08-aebf-e49c454dd9e4';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
